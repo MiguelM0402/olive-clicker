2b6e206ca646a20c9a63a77cb77dc01ed5bf2d6c
MiguelM0402 <gibbyboi011@gmail.com>
2025-01-15 12:57:17 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
+const tickRate = 1000 / 30;
 let score = 0;
 
-let patches = new Friend('Patches', 0.1, 15, 'buyPatches');
+let patches = new Building('Patches', 0.1, 15, 'buyPatches'); //patches
+let brownie = new Building('Brownie', 5, 200, 'buyBrownie'); //brownie
+let coco = new Building('Coco', 15, 500, 'buyCoco'); //coco 
+let angel = new Building('Angel', 50, 1000, 'buyAngel'); // angel
 
 function incScore() {
     score += patches.cps;
+    score += brownie.cps;
+    score += coco.cps;
+    score += angel.cps;
 }
 
-function scorePlusPlus(){
+function scorePlusPlus() {
     score++;
-    document.getElementById('score').innerHTML = score;
-    incScore();
 }
+
+function updateButtons() {
+    patches.buttonState();
+    brownie.buttonState();
+    coco.buttonState();
+    angel.buttonState();
+}
+
+function updatePage() {
+    incScore();
+    updateButtons();
+    document.getElementById('score').innerHTML =
+        '' + Math.floor(score).toLocaleString();
+}
+
+setInterval(updatePage, tickRate);
