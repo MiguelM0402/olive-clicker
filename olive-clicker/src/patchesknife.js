b235e91e96499ddec3567e434c6cee351ceea437
MiguelM0402 <gibbyboi011@gmail.com>
2025-02-05 13:01:29 -0500

    Add files via upload
    fixed PatchesKnife

---
+++ olive-clicker/src/patchesknife.js
@@ -0,0 +1,13 @@
+class PatchesKnife extends Upgrade {
+    constructor(upgradeName, cost, building){
+        super(upgradeName, cost);
+        this.building = building;
+    }
+
+    purchase() {
+        super.purchase();
+        clickStrength++;
+        this.building.doubleUpgrade++;
+        this.building.applyDoubleUpgrade();
+    }
+}
---
+++ olive-clicker/src/upgrade.js
@@ -0,0 +1,38 @@
+class Upgrade {
+    constructor(upgradeName, cost, imgPick) {
+        this.upgradeName = upgradeName;
+        this.cost = cost;
+        this.buttonId = 'buy' + upgradeName.replace(/\s/g, '');
+        this.owned = false;
+        this.visible = false
+        this.imgPick = imgPick;
+    }
+
+    purchase() {
+        score -= this.cost;
+        this.owned = true;
+    }
+
+    buttonState() {
+        if (this.owned) {
+            document.getElementById(this.buttonId).style.display = 'none';
+        }
+
+        if (!this.visible) {
+            document.getElementById(this.buttonId).style.display = 'none';
+            if (score >= this.cost) {
+                this.visible = true;
+                document.getElementById(this.buttonId).style.display =
+                    'initial';
+            }
+        }
+
+        if (score < this.cost) {
+            document.getElementById(this.buttonId).disabled = true;
+        } else {
+            document.getElementById(this.buttonId).disabled = false;
+        }
+
+        document.getElementById(this.buttonId).innerHTML =  "<img src='" + this.imgPick + "'>" + "<p>" + 'Buy ' + this.upgradeName + ' (Cost: ' + Math.ceil(this.cost).toLocaleString() + ' Olives)';
+    }
+}
